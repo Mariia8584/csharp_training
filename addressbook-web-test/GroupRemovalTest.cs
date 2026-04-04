83f14c86b931ae88f6ef38d12322ebb92b5751ea
Filippova Maria <fma8584@gmail.com>
2026-04-04 13:49:43 +0300

    lesson 2 task 5

---
+++ addressbook-web-test/GroupRemovalTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using static System.Net.WebRequestMethods;
+
+namespace WebAddressbookTests 
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void GroupRemovalTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            SlectGroup(1);
+            RemoveGroup();
+            ReturnToGroupsPage();
+        }
+    }
+}
