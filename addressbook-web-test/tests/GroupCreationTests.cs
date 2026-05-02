22f682d927d537f1fb772128fbb8977fa92b1914
Filippova Maria <fma8584@gmail.com>
2026-05-02 12:39:45 +0300

    lesson 6.1

--- addressbook-web-test/tests/GroupCreationTests.cs
+++ addressbook-web-test/tests/GroupCreationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,24 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach(string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+
+            }
+            return groups;
+        } 
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
---
+++ addressbool-test-data-generators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using WebAddressbookTests;
+
+namespace addressbool_test_data_generators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            for (int i = 0; i < count; i++) 
+            {
+                writer.WriteLine(String.Format("${0},${1},${2}",
+                     TestBase.GenerateRandomString(10), 
+                     TestBase.GenerateRandomString(100),
+                     TestBase.GenerateRandomString(100)));
+            }
+            writer.Close();
+        }
+    }
+}
