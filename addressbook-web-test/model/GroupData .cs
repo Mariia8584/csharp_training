de8898aae3da350d22082370a90b2a7efa3d2e74
Filippova Maria <fma8584@gmail.com>
2026-05-02 14:02:11 +0300

    lesson 6.2

--- addressbook-web-test/model/GroupData .cs
+++ addressbook-web-test/model/GroupData .cs
@@ -5,10 +5,15 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WebAddressbookTests
 {
     public class GroupData : IEquatable<GroupData>, IComparable<GroupData>
     {
+        public GroupData()
+        {
+        }
+
         public GroupData(string name)
         {
             Name = name;
--- addressbook-web-test/tests/GroupCreationTests.cs
+++ addressbook-web-test/tests/GroupCreationTests.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace WebAddressbookTests
 {
@@ -27,7 +29,7 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        public static IEnumerable<GroupData> GroupDataFromFile()
+        public static IEnumerable<GroupData> GroupDataFromCsvFile()
         {
             List<GroupData> groups = new List<GroupData>();
             string[] lines = File.ReadAllLines(@"groups.csv");
@@ -42,9 +44,17 @@ namespace WebAddressbookTests
 
             }
             return groups;
-        } 
+        }
 
-        [Test, TestCaseSource("GroupDataFromFile")]
+        public static IEnumerable<GroupData> GroupDataFromXmlFile()
+        {
+            return (List<GroupData>) 
+                new XmlSerializer(typeof(List<GroupData>))
+                    .Deserialize(new StreamReader(@"groups.xml"));
+        }
+
+
+        [Test, TestCaseSource("GroupDataFromXmlFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
