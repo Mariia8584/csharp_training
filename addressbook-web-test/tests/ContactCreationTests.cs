ef003db8c9db4d7d1a536ecca20fa9b661ba7c93
Filippova Maria <fma8584@gmail.com>
2026-05-04 14:04:20 +0300

    lesson 6 task 15

--- addressbook-web-test/tests/ContactCreationTests.cs
+++ addressbook-web-test/tests/ContactCreationTests.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace WebAddressbookTests
 {
@@ -24,11 +27,17 @@ namespace WebAddressbookTests
             return contacts;
         }
 
-        public static IEnumerable<ContactData> ContactDataFromXmlFile() 
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
         {
-            List<ContactData> contacts = new List<ContactData>();
+            return (List<ContactData>)
+                new XmlSerializer(typeof(List<ContactData>))
+                    .Deserialize(new StreamReader(@"contacts.xml"));
+        }
 
-            return contacts;
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(
+                File.ReadAllText(@"contacts.json"));
         }
 
         [Test, TestCaseSource("ContactDataFromXmlFile")]
