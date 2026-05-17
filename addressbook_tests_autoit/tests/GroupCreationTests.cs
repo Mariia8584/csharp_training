80fb6c51cd74b1bf331db3b3efc0e731be804bda
Filippova Maria <fma8584@gmail.com>
2026-05-17 15:18:51 +0300

    lesson 8.1

---
+++ addressbook_tests_autoit/tests/GroupCreationTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        /*[Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            
+            GroupData newGroup = new GroupData()
+            {
+                Name = "test"
+            };
+
+            app.Groups.Add(newGroup);
+            
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }*/
+
+        [Test]
+        public void TestGroupCreation()
+        {
+            int oldCount = app.Groups.GetGroupCount();
+
+            GroupData newGroup = new GroupData() { Name = "test" };
+            app.Groups.Add(newGroup);
+
+            int newCount = app.Groups.GetGroupCount();
+
+            Assert.AreEqual(oldCount + 1, newCount,
+                "Количество групп не увеличилось после создания новой группы");
+        }
+
+    }
+}
